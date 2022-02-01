e7afc22bed182a75093e20a35ca8bd71be034f16
MarianaBirca <82412276+MarianaBirca@users.noreply.github.com>
2022-02-01 11:29:22 +0000

    Added a search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            {" "}
+            This project was coded by Mariana Birca and is open-sourced on{" "}
+            <a
+              href="https://github.com/MarianaBirca/dictionary-project"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://dictionary-app-react-mb.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Netlify
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
